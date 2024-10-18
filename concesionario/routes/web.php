1be24914fea57da631edc9ed1174f8d11c18f417
Mblanco24 <marilynblanco1998@gmail.com>
2024-10-18 03:09:59 -0400

    punto y coma corregido

--- concesionario/routes/web.php
+++ concesionario/routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Mail\notification;
-use Illuminate\Support\Facades\Mail
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
